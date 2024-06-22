75bfba101b998db6eecdbbf34d02898bbeadfc9b
VijayWhat <vijayrv042@gmail.com>
2024-06-22 13:07:00 +0530

    scroll indicator added

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,10 @@
 import { Dropdown } from "bootstrap";
 
+// Function to update the scroll indicator width
+window.addEventListener('scroll', () => {
+    const scrollIndicator = document.getElementById('scrollIndicator');
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrollPercentage = (scrollTop / scrollHeight) * 100;
+    scrollIndicator.style.width = scrollPercentage + '%';
+});
