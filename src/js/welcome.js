49dcdf08154195cdec81431629ab1d7ee6ea4fa5
sathishk <sathishkumar.thiyagarajan@gmail.com>
2025-01-28 00:18:14 +0530

    Done

--- src/js/welcome.js
+++ src/js/welcome.js
@@ -1,59 +1,74 @@
 
-class WelcomePage{
-    constructor() {
-        console.log('Welcoe');
-        const params = new URLSearchParams(window.location.search);
+class WelcomePage {
+  constructor() {
 
-        const token = params.get("token");
-        console.log(token);
-        if (token) {
-            console.log("register token");
-            
-        }else{
-            console.log("no register token");
-            if (sessionStorage.getItem("reg_token")) {
-                this.register(
-                  sessionStorage.getItem("reg_token"),
-                  sessionStorage.getItem("profile_pic")
-                );
-              }
-        }
-        
+    const params = new URLSearchParams(window.location.search);
+
+    const token = params.get("token");
+    console.log(token);
+    if (token) {
+      console.log("register token");
+
+    } else {
+      console.log("no register token");
+      if (sessionStorage.getItem("reg_token")) {
+        this.register(
+          sessionStorage.getItem("reg_token"),
+          sessionStorage.getItem("profile_pic")
+        );
+      }
     }
-    register(registrationToken, profile_pic){
-        console.log(registrationToken,profile_pic);
 
-        document.querySelector("form").addEventListener("submit", (event) => {
-            event.token = registrationToken;
-            event.preventDefault();
-            // this.errorPane.classList.add("d-none");
-            let regRequest = {
-              name: document.querySelector("#name").value,
-              dob: document.querySelector("#dob").value,
-            };
+  }
 
-        fetch("/api/auth/register", {
-            method: "POST",
-            headers: {
-              "content-type": "application/json",
-              Authorization: "Bearer " + event.token,
-            },
-            body: JSON.stringify(regRequest),
-          }).then((response) =>{
-            if(!response.ok){
-                console.log("Not register");  
-            }
-          }).then((response) =>{
-            if(response.ok){
-                console.log("Register Successfully");
-                
-            }
-          }).catch(() =>{
-            console.log("Unable to register contact admin");
-            
-          })
-        });
+  register(registrationToken, profile_pic) {
+    console.log(registrationToken, profile_pic);
 
+    document.querySelector("form").addEventListener("submit", (event) => {
+      event.token = registrationToken;
+      event.preventDefault();
+      // this.errorPane.classList.add("d-none");
+      let regRequest = {
+        name: document.querySelector("#name").value,
+        dob: document.querySelector("#dob").value,
+      };
+
+      fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          Authorization: "Bearer " + event.token,
+        },
+        body: JSON.stringify(regRequest),
+      }).then((response) => {
+        if (!response.ok) {
+          this.showError("Unable to register. Please contact admin");
+        } else {
+          return response.json();
         }
+      })
+      .then((auth_response) => {
+        auth_response.expiresIn = Date.now() + auth_response.expiresIn;
+        sessionStorage.auth = JSON.stringify(auth_response);
+        this.reload();
+      }).catch(() => {
+        console.log("Unable to register contact admin");
+
+      })
+    });
+
+  }
+
+  reload() {
+    const refPage = sessionStorage.getItem("ref_page");
+    sessionStorage.removeItem("ref_page");
+    if (refPage) {
+      window.location.href = refPage;
+      window.location.replace(refPage);
+    } else {
+      window.location.href = "/";
+      window.location.replace("/");
+    }
+  }
 }
 new WelcomePage();
