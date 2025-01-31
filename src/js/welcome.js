98e3304f6a207d52bb22808e08642bc73dfde51f
Sobhan369 <sobhanbabukarri@gmail.com>
2025-01-31 14:56:48 +0530

    add message for login

--- src/js/welcome.js
+++ src/js/welcome.js
@@ -35,29 +35,62 @@ class WelcomePage {
         name: document.querySelector("#name").value,
         dob: document.querySelector("#dob").value,
       };
+    
 
-      fetch("/api/auth/register", {
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-          Authorization: "Bearer " + registrationToken,
-        },
-        body: JSON.stringify(regRequest),
-      }).then((response) => {
-        if (!response.ok) {
-          this.showError("Unable to register. Please contact admin");
-        } else {
-          return response.json();
-        }
-      })
-        .then((auth_response) => {
-          this.reload(refPage, auth_response);
-        }).catch(() => {
-          console.log("Unable to register contact admin");
+      const age = this.getAge(regRequest.dob);
 
+      if (age < 10 || age > 80) {
+        console.log("Please Enter valid Date of Birth");
+      } else {
+        fetch("/api/auth/register", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+            Authorization: "Bearer " + event.token,
+          },
+          body: JSON.stringify(regRequest),
         })
+          .then((response) => {
+            if (!response.ok) {
+              this.showError("Unable to register. Please contact admin");
+            } else {
+              return response.json();
+            }
+          })
+          .then((auth_response) => {
+            auth_response.expiresIn = Date.now() + auth_response.expiresIn;
+            sessionStorage.auth = JSON.stringify(auth_response);
+            this.reload();
+          })
+          .catch(() => {
+            this.showError("Unable to register. Please contact admin");
+          });
+      }
     });
 
+    
+
+  }
+
+  getAge(value) {
+    var selectedDate = new Date(value);
+    var now = new Date();
+
+    if (selectedDate > now) {
+      return -1;
+    }
+
+    //calculate month difference from current date in time
+    var month_diff = now - selectedDate.getTime();
+
+    //convert the calculated difference in date format
+    var age_dt = new Date(month_diff);
+
+    //extract year from date
+    var year = age_dt.getUTCFullYear();
+
+    //now calculate the age of the user
+    return Math.abs(year - 1970);
   }
 
   reload(refPage, auth_response) {
